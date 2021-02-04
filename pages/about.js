c702c39657e70493d379540d8a302e0fb7142115
1w3112089CardellinoFernandoAgustin <69600638+1w3112089CardellinoFernandoAgustin@users.noreply.github.com>
2021-02-04 20:26:24 -0300

    grammar correction

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ function About() {
     <Layout pageTitle="About | My Blog">
       <article className="prose max-w-none">
         <h1>About</h1>
-        <p>Hello, I'm a software developer student. In the last few month I'v been  different AI and Data Analysis courses, 
+        <p>Hello, I'm a software developer student. In the last few month I've been  different AI and Data Analysis courses, 
             so I decided to create my personal blog to share my progress and the projects I have completed.</p>
         <p>
             This Blog is a work in progress, so I'll be adding new things in the near feature. However if you wish to contact me, 
